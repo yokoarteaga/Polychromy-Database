f43caca3943841688e603c401aed514760e06ed0
yokoarteaga <yoko.arteaga@ntnu.no>
2026-06-23 18:32:31 +0200

    Update zonodo ID in main_page.py

--- main_page.py
+++ main_page.py
@@ -303,7 +303,8 @@ def on_query_change():
     sync_all_selection()
 
 def download_file_from_zenodo(name,recordID,ext):
-    key_ID = ["17828658", "17828914", "17897928"]
+    # key_ID = ["17828658", "17828914", "17897928"]
+    key_ID = ["17828658", "17828914", "20816883"]
 
     try:
         url = f"https://zenodo.org/records/{key_ID[0]}/files/{name}.{ext}?download=1"
